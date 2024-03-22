4f5fd0b64517c6de9e980011ca24f41eef53c403
Sergey Chupligin <neochapay@gmail.com>
2024-03-22 13:38:54 +0300

    Added ByteArrayResource

---
+++ src/http/bytearrayresource.cpp
@@ -0,0 +1,28 @@
+#include "bytearrayresource.h"
+namespace QtWebServer {
+
+namespace Http {
+
+    ByteArrayResource::ByteArrayResource(QString uniqueIdentifier
+                                         , QByteArray data
+                                         , QObject* parent)
+        : Resource(uniqueIdentifier, parent)
+        , m_data(data)
+    {
+        setContentType("text/plain");
+    }
+
+    ByteArrayResource::~ByteArrayResource()
+    {
+    }
+
+    void ByteArrayResource::deliver(const Request &request, Response &response)
+    {
+        if (request.method() == "get") {
+            response.setHeader(Http::ContentType, contentType());
+            response.setBody(m_data);
+            response.setStatusCode(StatusCode::Ok);
+        }
+    }
+}
+}
---
+++ src/http/bytearrayresource.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// Own includes
+#include "httpresource.h"
+
+// Qt includes
+#include <QIODevice>
+
+namespace QtWebServer {
+
+namespace Http {
+
+    class ByteArrayResource : public Resource {
+        Q_OBJECT
+
+        public:
+            ByteArrayResource(QString uniqueIdentifier,
+                              QByteArray data,
+                              QObject* parent = nullptr);
+
+            ~ByteArrayResource();
+
+            virtual void deliver(const Request& request, Response& response);
+
+        private:
+            QByteArray m_data;
+        };
+    }
+}

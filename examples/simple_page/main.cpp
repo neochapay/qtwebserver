8e351e16286d95d6333dd702f88153515ba049a4
Sergey Chupligin <neochapay@gmail.com>
2024-03-22 13:44:39 +0300

    Add example simple page

---
+++ examples/simple_page/main.cpp
@@ -0,0 +1,25 @@
+#include <QCoreApplication>
+#include <QFile>
+
+#include "tcp/tcpmultithreadedserver.h"
+#include "http/httpwebengine.h"
+#include "http/bytearrayresource.h"
+
+using namespace QtWebServer;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Tcp::MultithreadedServer s;
+    Http::WebEngine w;
+
+
+    w.addResource(new Http::ByteArrayResource(
+                      "/",
+                      QByteArray("Hello world")));
+
+    s.setResponder(&w);
+    s.listen(QHostAddress::Any, 3000);
+    return a.exec();
+}
